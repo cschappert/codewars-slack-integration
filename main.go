bd6a0534356b5fd7a89d925cd03806c9cc0af1bc
Chris Schappert <work.nonwork+gh@gmail.com>
2020-08-23 03:17:50 +0900

    listen for events from codewars, consume codewars api, send msg to slack

--- main.go
+++ main.go
@@ -1,29 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/slack-go/slack"
 )
 
-func main() {
-	// read access token from env var and create new client
-	api := slack.New(os.Getenv("SLACK_TOKEN"))
+var api *slack.Client
+var channelId string
+var codewarsIconUrl string = "https://www.codewars.com/assets/logos/logo-square-paper-bg-c3d2b1eb4fb35d75b0c0c0e3b74616fab527afdce9d1d3184624cf0b4e950357.jpg"
 
-	// get channel ID from env var. bot must already be invited to room.
-	channelId := os.Getenv("CHANNEL_ID")
+type ChallengeDetail struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
 
-	// send message to channel by ID with variable options.
-	// TODO: is it possible to send to all joined / invited channels?
+type CodeChallenge struct {
+	Id          string `json:"id"`
+	CreatedById string `json:"created_by_id"`
+}
+
+type Solution struct {
+	Id     string `json:"id"`
+	UserId string `json:"user_id"`
+}
+
+type Event struct {
+	Action        string         `json:"action"`
+	CodeChallenge *CodeChallenge `json:"code_challenge,omitempty"`
+	Solution      *Solution      `json:"solution,omitempty"`
+}
+
+func getChallengeDetail(challenge string) *ChallengeDetail {
+	url := fmt.Sprintf("https://www.codewars.com/api/v1/code-challenges/%s", challenge)
+	res, _ := http.Get(url)
+	resBody, _ := ioutil.ReadAll(res.Body)
+
+	var challengeDetail ChallengeDetail
+	json.Unmarshal(resBody, &challengeDetail)
+
+	return &challengeDetail
+}
+
+func postMessage(user, challenge string) {
+	cd := getChallengeDetail(challenge)
+	msg := fmt.Sprintf("User *%s* completed coding challenge *%s*\nTry it! %s", user, cd.Name, cd.Url)
 	_, _, _, err := api.SendMessage(
 		channelId,
-		slack.MsgOptionText("Mama Mia!", false),
-		slack.MsgOptionUsername("The Pizza Man"),
-		slack.MsgOptionIconEmoji(":pizza:"),
+		slack.MsgOptionText(msg, false),
+		slack.MsgOptionUsername("Codewars"),
+		slack.MsgOptionIconURL(codewarsIconUrl),
 	)
 
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+// Return 200
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK!")
+}
+
+// Handle Codewars events
+func CodewarsHookHandler(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var event Event
+	json.Unmarshal(reqBody, &event)
+
+	if event.Action == "solution_finalized" {
+		fmt.Println("enter")
+		postMessage(event.Solution.UserId, event.CodeChallenge.Id)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK")
+}
+
+func main() {
+	// create slack API client
+	api = slack.New(os.Getenv("SLACK_TOKEN"))
+	channelId = os.Getenv("CHANNEL_ID")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", RootHandler)
+	r.HandleFunc("/hook/codewars", CodewarsHookHandler)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
